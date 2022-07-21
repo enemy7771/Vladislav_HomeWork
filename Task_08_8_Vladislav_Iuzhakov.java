d18dc3cf51d55f3652b5a2f897fa906bdaecb2f3
enemy7771 <108586951+enemy7771@users.noreply.github.com>
2022-07-21 12:07:15 +0300

    Create Task_08_8_Vladislav_Iuzhakov.java

---
+++ Task_08_8_Vladislav_Iuzhakov.java
@@ -0,0 +1,18 @@
+/**Задача 10. Кон, котэ и нация
+ "Кон", "котэ" и "нация". Как соединить эти три строки? Ты же помнишь, как соединять String'и в Java?
+ Исправь ошибки в коде, чтобы программа компилировалась и работала.
+
+
+ Requirements:
+ 1. В методе main класса Solution нужно исправить ошибки: при выводе на экран должна использоваться конкатенация строк
+ */
+
+
+
+public class Task_08_8_Vladislav_Iuzhakov {
+        public static void main(String[] args) {
+        String name = "Diego";
+        int age = 35;
+        System.out.println("Name : " + name + " Age : " + age);
+    }
+}
