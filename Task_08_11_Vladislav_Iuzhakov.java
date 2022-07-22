8bb0c463d64fbe803129ecbc949e09c4535b58a9
enemy7771 <108586951+enemy7771@users.noreply.github.com>
2022-07-22 17:01:05 +0300

    Create Task_08_11_Vladislav_Iuzhakov.java

---
+++ Task_08_11_Vladislav_Iuzhakov.java
@@ -0,0 +1,14 @@
+/**Следим за скобками
+ Кажется, у нас есть лишняя скобка? Или одной не хватает? Разберись-ка с этой проблемой.
+
+ Requirements:
+ 1. В методе main класса Solution нужно исправить ошибки компиляции.
+ */
+
+public class Task_08_11_Vladislav_Iuzhakov {
+    public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            System.out.println("Windows detected!");
+        }
+    }
+}
